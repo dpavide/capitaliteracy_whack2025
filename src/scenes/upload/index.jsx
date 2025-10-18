beb3c15c096cd4cc0a45fddc216fc4c3046bf4a3
Muhammad Taimor Alam <alamtaimor@gmail.com>
2025-10-18 23:10:53 +0100

    change port

--- src/scenes/upload/index.jsx
+++ src/scenes/upload/index.jsx
@@ -154,7 +154,7 @@ const FileUpload = () => {
         formData.append('style', 'formal');
 
         try {
-          const res = await fetch('http://127.0.0.1:5000/upload', {
+          const res = await fetch('http://127.0.0.1:5173/upload', {
             method: 'POST',
             body: formData,
           });
